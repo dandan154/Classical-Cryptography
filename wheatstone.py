652ba0ba2217d5c396b6e631a8247d1d13811cdc
danieljmc <dmcguinness@dundee.ac.uk>
2017-12-20 15:35:35 +0000

    Simplified alphabet matrix into 1D list

--- wheatstone.py
+++ wheatstone.py
@@ -5,18 +5,20 @@ from collections import OrderedDict
 
 #Assign cipher key
 keyword = "PLAYFAIREXAMPLE"
-keyword = keyword.upper()
+#keyword = keyword.upper()
 
 #Define base alphabet matrix
-alphamat = [['A', 'B', 'C', 'D', 'E'],
-['F','G','H','I','K'],
-['L','M','N','O','P'],
-['Q','R','S','T','U'],
-['V','W','X','Y','Z']]
+alphamat = ['A','B','C','D','E',
+'F','G','H','I','K',
+'L','M','N','O','P',
+'Q','R','S','T','U',
+'V','W','X','Y','Z']
 
-dim = 5
 
-size = sum(len(x) for x in alphamat)
+dim = 5     #key table matrix width
+buf = 'X'   #buffer character for ciphertext preparation
+
+#size = sum(len(x) for x in alphamat)
 
 #prepare cipher matrix with initial values
 cipmat = deepcopy(alphamat)
@@ -24,35 +26,36 @@ cipmat = deepcopy(alphamat)
 #remove repeated characters from keyword
 unqkey = ''.join(OrderedDict.fromkeys(keyword))
 
+#create list from key
+key = list(unqkey)
 
 #Assign unique letters of key to cipher matrix
 i=0
-for x in range(0,dim):
-    for y in range(0, dim):
-        if i < len(unqkey):
-            cipmat[x][y] = unqkey[i]
-            i += 1
+for x in range(0,len(unqkey)):
+            cipmat[x] = unqkey[x]
 
 #for x in range(i,size)
 
 
 #Get user plaintext to be encrypted
 plntxt = input("Enter the message you wish to encrypt: ")
+
+#Prepare plaintext for ciphering - remove whitespace and capitalize letters
 plntxt = plntxt.replace(" ", "")
 plntxt = plntxt.upper()
 ciptxt = list(plntxt)
 
-#Insert X between repeated letters
+#Insert buffer between repeated letters
 for x in range(1, len(ciptxt)):
     if ciptxt[x] == ciptxt[x-1]:
-        ciptxt.insert(x,'X')
+        ciptxt.insert(x,buf)
 
 #ensure that blocks of 2 can be created
 if (len(ciptxt)%2) == 1:
-    ciptxt.append('X')
+    ciptxt.append(buf)
 
 ###DEBUGGING###
-#print(cipmat)
+print(cipmat)
 #print(alphamat)
 #print(size)
 #print(unqkey)
