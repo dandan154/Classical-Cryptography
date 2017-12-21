52f596240d976eb7bb0828f75c19d352f5ec6eba
danieljmc <dmcguinness@dundee.ac.uk>
2017-12-21 18:05:48 +0000

    removed broken method

--- railfence.py
+++ railfence.py
@@ -1,32 +1,9 @@
 #!/usr/bin/python3
 
-from collections import OrderedDict
-
 def enc(plntxt, rail):
-    plntxt = list(plntxt)
-    cyc = (rail*2)-2        #number of characters in a cycle
-    ciptxt=[]
-
-    #Add characters to ciphertext based on the rail that they are on
-    for x in range (0, rail):
-        for y in range (0, len(plntxt)):
-            #Add character to cipher if it matches current rail
-            if (y%cyc == x):
-                ciptxt.append(plntxt[y])
-            #Work back up rails and add appropriate characters
-            elif(y % cyc > (rail-1)):
-                if(y%(rail-1) == x):
-                    ciptxt.append(plntxt[y])
-
-    return ciptxt
-
+    print("enc")
 def dec(ciptxt, rail):
-
-    size =len(ciptxt)               #number of characters in ciphertext
-    railwidth = int(length/rail)    #number of cycles that occur
-    cyc = (rail*2)-2                #number of characters in a cycle
-
-    plntxt=[]
+    print("dec")
 
 def menu():
     print("""===RAIL FENCE CIPHER===
