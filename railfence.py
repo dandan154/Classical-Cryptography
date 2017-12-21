c53e6eaa03a1140c2d63921980a2bb3b71bbf3f8
danieljmc <dmcguinness@dundee.ac.uk>
2017-12-21 00:27:47 +0000

    encryption now working

--- railfence.py
+++ railfence.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 
+from collections import OrderedDict
+
+def enc(plntxt, rail):
+    plntxt = list(plntxt)
+    cyc = (rail*2)-2
+    ciptxt=[]
+
+    for x in range (0, rail):
+        print(x)
+        for y in range (0, len(plntxt)):
+            if (y%cyc == x):
+                ciptxt.append(plntxt[y])
+            elif(y % cyc > (rail-1)):
+                if(y%(rail-1) == x):
+                    ciptxt.append(plntxt[y])
+
+    print("Ciphertext: " + ''.join(ciptxt))
+
 def menu():
     print("""===RAIL FENCE CIPHER===
     1. Encrypt
@@ -11,7 +29,8 @@ def menu():
         choice = int(input("please select: "))
 
         if(choice == 1):
-            print(1)
+            enc("WEAREDISCOVEREDFLEEATONCE",3)
+            #WECRLTEE RDSOEEFEAOCAIVDEN
         elif(choice == 2):
             print(2)
 
@@ -19,3 +38,5 @@ def menu():
             print("goodbye!")
         else:
             print("please try again!")
+
+menu()
