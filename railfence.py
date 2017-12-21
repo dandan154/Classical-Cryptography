032520cbeb44ff5ad9bc282c4ccdc8f6d00fbb0b
danieljmc <dmcguinness@dundee.ac.uk>
2017-12-21 13:44:04 +0000

    added decryption method

--- railfence.py
+++ railfence.py
@@ -4,19 +4,29 @@ from collections import OrderedDict
 
 def enc(plntxt, rail):
     plntxt = list(plntxt)
-    cyc = (rail*2)-2
+    cyc = (rail*2)-2        #number of characters in a cycle
     ciptxt=[]
 
+    #Add characters to ciphertext based on the rail that they are on
     for x in range (0, rail):
-        print(x)
         for y in range (0, len(plntxt)):
+            #Add character to cipher if it matches current rail
             if (y%cyc == x):
                 ciptxt.append(plntxt[y])
+            #Work back up rails and add appropriate characters
             elif(y % cyc > (rail-1)):
                 if(y%(rail-1) == x):
                     ciptxt.append(plntxt[y])
 
-    print("Ciphertext: " + ''.join(ciptxt))
+    return ciptxt
+
+def dec(ciptxt, rail):
+
+    size =len(ciptxt)               #number of characters in ciphertext
+    railwidth = int(length/rail)    #number of cycles that occur
+    cyc = (rail*2)-2                #number of characters in a cycle
+
+    plntxt=[]
 
 def menu():
     print("""===RAIL FENCE CIPHER===
@@ -30,10 +40,8 @@ def menu():
 
         if(choice == 1):
             enc("WEAREDISCOVEREDFLEEATONCE",3)
-            #WECRLTEE RDSOEEFEAOCAIVDEN
         elif(choice == 2):
-            print(2)
-
+            dec("WECRLTEERDSOEEFEAOCAIVDEN",3)
         elif(choice == 0):
             print("goodbye!")
         else:
