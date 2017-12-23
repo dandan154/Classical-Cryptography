772124d71d9ff3965ab3a5faffb0639fe8883c34
danieljmc <dmcguinness@dundee.ac.uk>
2017-12-23 17:40:43 +0000

    Added Nihlist encryption + addition comments

--- polybius.py
+++ polybius.py
@@ -32,8 +32,11 @@ def keyword_shift(keyword, square):
 	Create polybius square using keyword.
 
 	Arguments:
-	keyword -- string value by which a polybius square is shifted by
+	keyword -- keyword that shifts the square - string
 	square -- polybius square - character list
+
+	Returns:
+	shift -- shifted polybius square - character list
     """
 	#Get dimensions of polybius square
 	dim = int(math.sqrt(len(square)))
@@ -73,8 +76,11 @@ def enc(plntxt,square):
 	Encrypt a string using a given polybius square.
 
 	Arguments:
-	plntxt -- string value of text that is to be encrypted via the square
+	plntxt -- text that is to be encrypted via the square - string
 	square -- polybius square - character list
+
+	Returns:
+	ciptxt --ciphered plaintext - character list
     """
 	#Get dimensions of polybius square
 	dim = int(math.sqrt(len(square)))
@@ -98,33 +104,37 @@ def dec(ciptxt, square):
 	Decrypt a ciphertext string using a given polybius square.
 
 	Arguments:
-	ciptxt -- ciphertext to be converted through the use of the square
+	ciptxt -- ciphertext to be converted through the use of the square - string
 	square -- polybius square - character list
+
+	Returns:
+	plntxt -- decrypted ciphertext - character list
    	"""
 	#Get dimensions of polybius square
 	dim = int(math.sqrt(len(square)))
 
 	#Parse through ciphertext and convert to characters
-	tmp=[]
+	plntxt=[]
 	for x in range(0, int(len(ciptxt)/2)):
 
 		#Get individual values of coordinates & convert to index lookup
 		ind = (int(ciptxt[(x*2)]) - 1) * dim + (int(ciptxt[x*2 + 1]) - 1)
 
 		#Add new character to decrypted plaintext
-		tmp.append(square[ind])
+		plntxt.append(square[ind])
 
-	ciptxt = tmp
-
-	return ciptxt
+	return plntxt
 
 def bifid_enc(plntxt,square):
 	"""
 	Encrypt a plaintext string using a polybius square via Bifid encryption.
 
 	Arguments:
-	plntxt -- plaintext to be ciphered
+	plntxt -- plaintext to be ciphered - string
 	square -- polybius square - character list
+
+	Returns:
+	ciptxt -- ciphered plaintext - string
 	"""
 	#Run plaintext through standard polybius encryption to get coordinates
 	ciptxt = enc(plntxt, square)
@@ -157,8 +167,11 @@ def bifid_dec(ciptxt,square):
 	Decrypt a ciphertext string using a polybius square via Bifid decryption.
 
 	Arguments:
-	ciptxt -- ciphertext to be decrypted
+	ciptxt -- ciphertext to be decrypted - string
 	square -- polybius square - character list
+
+	Returns:
+	plntxt -- decrypted plaintext - string
 	"""
 	#Run ciphertext through standard polybius encryption to get coordinates
 	plntxt = enc(ciptxt, square)
@@ -178,5 +191,72 @@ def bifid_dec(ciptxt,square):
 
 	return plntxt
 
+def nihlist_enc(plntxt, key, square):
+	"""
+	Encrypt a plaintext string using a polybius square via Nihlist ciphering.
 
-print(bifid_dec(bifid_enc("ABCDE", polybius_five), polybius_five))
+	Arguments:
+	plntxt -- plaintext to be encrypted - string
+	key --	used to encrypt text - string
+	square -- polybius square - character list
+
+	Returns:
+	ciptxt -- list of encypted characters - integer list
+	"""
+	plen = len(plntxt)	#plaintext length
+	klen = len(key)		#key length
+
+	#Repeat key to match plaintext length
+	if(plen > klen):
+		key = (key * (int(plen/klen) + 1))
+		key = key[0:plen]
+
+	#Get polybius square integer representations of key and plaintext
+	ciptxt = list(map(int, enc(plntxt, square)))
+	keylist = list(map(int, enc(key, square)))
+
+	#Add key and plaintext polybius integer values together
+	for x in range(0, plen):
+		ciptxt[x] = ciptxt[x] + keylist[x]
+
+	return ciptxt
+
+def nihlist_dec(ciptxt, key, square):
+	"""
+	Decrypt a ciphertext string using a polybius square via Nihlist decryption.
+
+	Arguments:
+	ciptxt -- list of ciphertext to be decrypted - integer list
+	key -- used to decrypt text - string
+	square -- polybius square - character list
+
+	Returns:
+	plntxt -- decrypted plaintext - string
+	"""
+	clen = len(ciptxt)	#ciphertext length
+	klen = len(key)		#key length
+
+	#Repeat key to match ciphertext length
+	if(clen > klen):
+		key =  (key * (int(clen/klen) + 1))
+		key = key[0:clen]
+
+	#Prepare key for arithmetic
+	keylist = list(map(int, enc(key, square)))
+
+	#Get original plaintext values
+	for x in range(0, clen):
+		ciptxt[x] = ciptxt[x] - keylist[x]
+
+	#Convert plaintext integers to characters
+	ciptxt = list(map(str, ciptxt))
+	plntxt = dec("".join(ciptxt), square)
+	plntxt = "".join(plntxt)
+
+	return plntxt
+###TESTING###
+#print(nihlist_dec([37, 106, 62, 36, 67, 47, 86, 26, 104, 53, 62, 77, 27, 55, 57, 66, 55, 36, 54, 27]
+#,"RUSSIAN", keyword_shift("ZEBRAS", polybius_five)))
+
+#print(enc("THE",polybius_five))
+#print(dec("".join(enc("THE",polybius_five)), polybius_five))
