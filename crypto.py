5a634f7303af68f026995b978e2d1b94ec7f0ce3
danieljmc <dmcguinness@dundee.ac.uk>
2017-12-28 11:49:46 +0000

    Created command line interface for currently implemented encryption schemes

---
+++ crypto.py
@@ -0,0 +1,96 @@
+import polybius
+import playfair
+import caesar
+
+import argparse
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("text", help="The text to be ran through the encryption scheme", type=str)
+parser.add_argument("-k", "--key", help="Insert the key used to decrypt or encrypt the given string", default="", type=str)
+parser.add_argument("-e", "--encrypt", help="perform an encryption function", action="store_true")
+parser.add_argument("-d", "--decrypt", help="perform a decryption function", action="store_true")
+parser.add_argument("-c", "--cipher", choices=['caesar', 'polybius', 'bifid', 'nihlist', 'trifid', 'playfair'], default='caesar', help="defines the encryption scheme to use when processing key and text", type=str)
+parser.add_argument("-s", "--square", help="defines the keyword by which a polybius square/trifid cube is shifted", default="", type=str)
+parser.add_argument("-g", "--group", help="defines the size of the stages in which a plaintext is ciphered (trifid only)", default=0, type=int)
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+args = parser.parse_args()
+
+
+
+def enc_list(text, key, cipher, square, group):
+    output=""
+    #Encryption Methods
+    if cipher == "caesar":
+        try:
+            key=int(key)
+            output = "".join(caesar.enc(list(text), key))
+        except ValueError:
+            output="Incorrect key value entered: for caesar ciphers, use an integer key"
+
+    elif cipher == "polybius":
+        output = " ".join(polybius.enc(text, (polybius.keyword_shift(square, polybius.polybius_five))))
+
+    elif cipher == "bifid":
+        output = "".join(polybius.bifid_enc(text, (polybius.keyword_shift(square, polybius.polybius_five))))
+
+    elif cipher == "trifid":
+        output = polybius.trifid_enc(text, polybius.keyword_shift(square, polybius.trifid), group)
+
+    elif cipher =="nihlist":
+        output= " ".join(polybius.nihlist_enc(text, key, polybius.keyword_shift(square, polybius.polybius_five)))
+
+    elif cipher == "playfair":
+        print("playfair")
+    return output
+
+def dec_list(text, key, cipher, square, group):
+    output=""
+    #Encryption Methods
+    if cipher == "caesar":
+        try:
+            key=int(key)
+            output = "".join(caesar.dec(list(text), key))
+        except ValueError:
+            output="Incorrect key value entered: for caesar ciphers, use an integer key"
+
+    elif cipher == "polybius":
+        output = "".join(polybius.dec(text, (polybius.keyword_shift(square, polybius.polybius_five))))
+
+    elif cipher == "bifid":
+        output = "".join(polybius.bifid_dec(text, (polybius.keyword_shift(square, polybius.polybius_five))))
+
+    elif cipher == "trifid":
+        output = polybius.trifid_dec(text, polybius.keyword_shift(square, polybius.trifid), group)
+
+    elif cipher =="nihlist":
+        output= " ".join(polybius.nihlist_dec(text, key, polybius.keyword_shift(square, polybius.polybius_five)))
+
+    elif cipher == "playfair":
+        print("playfair")
+
+    return output
+
+
+
+output=""
+if args.verbose:
+
+    #Display encryption scheme
+    print("ENCRYPTION: " + (args.cipher).upper())
+
+    #Display key value
+    if args.key != "":
+        print("KEY: " + args.key)
+
+    #Display group size
+    if args.group > 0:
+        print("GROUPING: " + str(args.group))
+
+    output += "CIPHERTEXT: "
+
+if args.decrypt:
+    output += dec_list(args.text, args.key, args.cipher, args.square, args.group)
+else:
+    output += enc_list(args.text, args.key, args.cipher, args.square, args.group)
+
+print(output)
